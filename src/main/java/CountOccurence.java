8f2e535a974119fd0c7146d80ea11eb33587d201
Dixita Lad <dixita-lad@users.noreply.github.com>
2018-08-31 19:27:47 -0400

    count occurence added

---
+++ src/main/java/CountOccurence.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountOccurence {
+    public static void main(String[] args) {
+        int[] listOfNumbers= {1, 2, 3, 3, 3, 4, 4, 5, 6, 7, 8, 9};
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for(int i: listOfNumbers) {
+            if(count.containsKey(i)) {
+                count.put(i, count.get(i) + 1);
+            }
+            else {
+                count.put(i, 1);
+            }
+        }
+
+        count.forEach((k,v) -> System.out.println(k + " : " + v));
+    }
+}
